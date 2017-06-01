279208da79972bd1057fe44cd29d2f1fee95e28d
Kevin <1041874421@qq.com>
2017-06-01 13:18:47 +0800

    抽离SignInForm组件

--- src/SignInForm.js
+++ src/SignInForm.js
@@ -1,20 +1,14 @@
 import React, { Component } from "react";
 
-export default class SignUpForm extends Component {
+export default class SignInForm extends Component {
     render() {
         return (
-            <form className="signUp" onSubmit={this.props.onSubmit.bind(this)}>
-                {/*注册*/}
-                <div className="row">
-                    <label>邮箱</label>
-                    <input type="text" value={this.props.formData.email}
-                        onChange={this.props.onChange.bind(null, "email")} />
-                </div>
+            <form className="signIn" onSubmit={this.props.onSubmit}>
+                {/*登录*/}
                 <div className="row">
                     <label>用户名</label>
                     <input type="text" value={this.props.formData.username}
                         onChange={this.props.onChange.bind(null, "username")} />
-                    {/* bind 不仅可以绑定 this，还可以绑定第一个参数 */}
                 </div>
                 <div className="row">
                     <label>密码</label>
@@ -22,7 +16,8 @@ export default class SignUpForm extends Component {
                         onChange={this.props.onChange.bind(null, "password")} />
                 </div>
                 <div className="row actions">
-                    <button type="submit">注册</button>
+                    <button type="submit">登录</button>
+                    <a href="#" onClick={this.props.onForgotPassword}>忘记密码？</a>
                 </div>
             </form>
         )
--- src/UserDialog.js
+++ src/UserDialog.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import "./UserDialog.css"
 import { signUp, signIn, sendPasswordResetEmail } from "./leanCloud"
 import SignUpForm from './SignUpForm'
+import SignInForm from './SignInForm'
 
 export default class UserDialog extends Component {
     constructor(props) {
@@ -97,27 +98,6 @@ export default class UserDialog extends Component {
     }
 
     render() {
-        
-
-        let signInForm = (
-            <form className="signIn" onSubmit={this.signIn.bind(this)}>
-                {/*登录*/}
-                <div className="row">
-                    <label>用户名</label>
-                    <input type="text" value={this.state.formData.username}
-                        onChange={this.changeFormDate.bind(this, "username")} />
-                </div>
-                <div className="row">
-                    <label>密码</label>
-                    <input type="password" value={this.state.formData.password}
-                        onChange={this.changeFormDate.bind(this, "password")} />
-                </div>
-                <div className="row actions">
-                    <button type="submit">登录</button>
-                    <a href="#" onClick={this.showForgotPassword.bind(this)}>忘记密码？</a>
-                </div>
-            </form>
-        )
         let signInOrSignUp = (
             <div className="signInOrSignUp">
                 <nav>
@@ -139,7 +119,12 @@ export default class UserDialog extends Component {
                     <SignUpForm formData={this.state.formData}  
                             onSubmit={this.signUp.bind(this)}
                             onChange={this.changeFormDate.bind(this)}/> : null}
-                    {this.state.selected === 'signIn' ? signInForm : null}
+                    {this.state.selected === 'signIn' ? 
+                    <SignInForm 
+                        formData={this.state.formData} 
+                        onSubmit={this.signIn.bind(this)}
+                        onchange={this.changeFormDate.bind(this)} 
+                        onForgotPassword={this.showForgotPassword.bind(this)}/> : null}
                 </div>
             </div>
         )
